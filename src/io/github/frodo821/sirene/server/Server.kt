317f9a0aef93b524793d1ca872f45bc21db24cd3
frodo821 <sakaic2003@gmail.com>
2019-01-08 20:24:09 +0900

    リモートサーバーをBluetoothからHTTPに書き換え

--- src/io/github/frodo821/sirene/server/Server.kt
+++ src/io/github/frodo821/sirene/server/Server.kt
@@ -1,88 +1,86 @@
 package io.github.frodo821.sirene.server
 
-import io.reactivex.Emitter
-import io.reactivex.Observable
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.async
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpServer
+import io.github.frodo821.sirene.application.MainUIController
+import javafx.application.Platform
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+/* import kotlinx.coroutines.* */
+import java.io.PrintWriter
 import java.net.InetSocketAddress
-import java.net.ServerSocket
-import java.net.SocketException
 import java.nio.charset.Charset
+import java.util.concurrent.Executors
 
-class Server: AutoCloseable {
-
+class Server(val host: String, val port: Int, private val ctl: MainUIController): AutoCloseable {
     companion object {
-        const val EOT = '\u0004'
+        val RPCs = mutableMapOf<String, Procedure>()
     }
 
-    private val server: ServerSocket = ServerSocket()
-    private var _running = false
-    val messager: Observable<RemoteControlMessenger>
-    private val emitters = mutableListOf<Emitter<RemoteControlMessenger>>()
+    private val server: HttpServer = HttpServer.create(InetSocketAddress(host, port), 0)
+    private val httpThreadPool = Executors.newFixedThreadPool(8)
+    private val serverJob: Job
+    private val __onClosed = mutableListOf<()->Unit>()
+    var onClosed: () -> Unit = {}
+        set (value) {__onClosed.add(value)}
+    val isAlive get() = serverJob.isActive
 
     init {
-        messager = Observable.create { emitters.add(it) }
+        server.apply {
+            executor = httpThreadPool
+            createContext("/", fun(it: HttpExchange) {
+                if(it.requestMethod.toUpperCase() != "POST") {
+                    it.responseHeaders.add("Content-Type", "text/plain")
+                    it.sendResponseHeaders(400, 0)
+                    PrintWriter(it.responseBody).use { out ->
+                        out.println("Expected method is POST, but received ${it.requestMethod}")
+                    }
+                    return
+                }
+
+                val segs = it.requestURI.toString().split('/').filter { k -> k.isNotEmpty() }
+                val body = it.requestBody.readBytes().toString(Charset.forName("UTF-8"))
+                val rpc = RPCs[if(segs.isNotEmpty()) segs[0] else "index"]
+
+                if(rpc == null) {
+                    it.responseHeaders.add("Content-Type", "text/plain")
+                    it.sendResponseHeaders(404, 0)
+                    PrintWriter(it.responseBody).use { out ->
+                        out.println("Unknown RPC Method: ${segs[0]}")
+                    }
+                    return
+                }
+
+                rpc(it, segs.drop(1), body, ctl)
+            })
+        }
+
+        setupProcedures()
+        serverJob = GlobalScope.async { start() }
     }
 
-    fun accept() = async(CommonPool) {
-        try {
-            server.bind(InetSocketAddress("0.0.0.0",4567))
-            _running = true
-            serve()
-            true
-        }catch (e: Exception) {
-            if(e is SocketException) {
-                println("User cancelled while waiting client to serve.")
-                true
-            } else {
-                e.printStackTrace()
-                false
-            }
+    private fun start() {
+        Platform.runLater {
+            ctl.connectStatus.text = "Listening on $host:$port..."
         }
-    }
-
-    private fun serve()
-    {
-        val cache = mutableListOf<Pair<String, Boolean>>()
-        val s = server.accept()
-        val ins = s.getInputStream()
-        val out = s.getOutputStream()
-        emitters.forEach { it.onNext(RemoteControlMessenger.beginMessage) }
-
-        while (s.isConnected) {
-            if(ins.available() < 1) continue
-
-            val str = ins.readBytes(ins.available()).toString(Charset.forName("UTF-8"))
-            val requests = str.split(EOT).toMutableList()
-            val lasti = cache.lastIndex
-
-            if(!cache[lasti].second)
-            {
-                val first = (cache.last().first + requests.removeAt(0))
-                emitters.forEach { it.onNext(RemoteControlMessenger.parse(first, out)) }
-                cache[lasti] = first to true
-            }
-
-            val last = requests.removeAt(requests.lastIndex)
-
-            requests.forEach { item ->
-                cache.add(item to true)
-                emitters.forEach { it.onNext(RemoteControlMessenger.parse(item, out)) }
-            }
-
-            if(str.endsWith(EOT)) {
-                cache.add(last to true)
-                emitters.forEach { it.onNext(RemoteControlMessenger.parse(last, out)) }
-            } else {
-                cache.add(last to false)
-            }
-        }
-        emitters.forEach { it.onNext(RemoteControlMessenger.finishMessage) }
+        server.start()
     }
 
     override fun close() {
-        server.close()
-        _running = false
-        emitters.forEach { it.onComplete() }
+        Platform.runLater {
+            ctl.connectStatus.text = "未接続です"
+        }
+        server.stop(1)
+        httpThreadPool.shutdown()
+        runBlocking {
+            serverJob.cancel()
+            serverJob.join()
+        }
+        __onClosed.forEach { it() }
+        __onClosed.clear()
     }
+
+    override fun toString() = "<SRCP Server> [$host:$port]"
 }
